6ff5c2fcaf6ed048f534b179ca6e8c26370c8d74
KhoaKhoa1234 <162591794+KhoaKhoa1234@users.noreply.github.com>
2024-03-08 08:04:49 +0700

    Add files via upload

---
+++ present.js
@@ -0,0 +1,45 @@
+
+var to = 'Hương!';
+var gift_url = 'https://www.facebook.com/hao.david.336?mibextid=ZbWKwL';
+var gift_image_url = 'eb4b97bf5dcef190a8df.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
